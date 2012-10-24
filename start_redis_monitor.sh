ae5e566d1c54f1f4c265088a018bcba8f5ef8387
wtraspadini <wtraspadini@8ee5f726-1d87-41fc-81a9-f71e053a05a0>
2012-10-24 09:07:42 +0000

    script to start the python process in background and stop it with its children
    git-svn-id: http://trsttools.online.gruppo24.net/home/svn/repositorysvn/python-projects/RedisFailover/trunk@8518 8ee5f726-1d87-41fc-81a9-f71e053a05a0

--- start_redis_monitor.sh
+++ start_redis_monitor.sh
@@ -1,1 +1,6 @@
-python redis_monitor.py -p /redis/cluster -z localhost:2181,localhost:2182 -r localhost:6379,localhost:6389,localhost:6399 -s 30
+#!/bin/bash
+
+PROGRAM="nohup python redis_monitor.py -p /siac/redis/cluster -z localhost:2181,localhost:2182 -r localhost:6379,localhost:6389,localhost:6399 -s 30"
+$PROGRAM &
+PID=$!
+echo $PID > process.pid
