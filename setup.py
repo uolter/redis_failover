00dbd27144fab5738bd2e9705804cba7f3940dd3
Maurizio Turatti <maurizio.turatti@gmail.com>
2012-11-06 10:45:05 +0100

    updated setup.py and requirements.txt

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from distutils.core import setup
 setup(name='redis_failover',
    version='1.0',
-   requires = ["redis (>=2.7.1)", "zc.zk (>=2.4.4)", "zc_zookeeper_static (>=1.0.0)", "zktools (>=0.2.1)"],
+   requires = ["redis (>=2.7.1)", "zc.zk (>=2.4.4)", "zc_zookeeper_static (>=1.0.0)", "zktools (>=0.3)"],
    description='redis failover client with Apache Zookeeper integration',
    packages=['redis_failover'],
 )
