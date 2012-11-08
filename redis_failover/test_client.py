d35e655517e52450becd9a181d4755b5029aca10
uolter <wtraspad@yahoo.it>
2012-11-08 12:31:20 +0100

    introduced the possibility to use different redis databeses with the failover client

--- redis_failover/test_client.py
+++ redis_failover/test_client.py
@@ -11,24 +11,36 @@ from redis.exceptions import ConnectionError
 from client import RedisFailover
 
 
-class RedisMock():
+class ConnectionPoolMock:
     
-    def __init__(self, host='localhost', port=6379, connection_pool = None):
+    def __init__(self,host='localhost', port=6379, db=0):
         
         self.host = host
         self.port = port
-        self.repository = {'ok':'ok'}
-        self.connection_pool = connection_pool
+        self.db = db
+
+class RedisMock():
+    
+    def __init__(self, host='localhost', port=6379, connection_pool = None, db=0):
+        
+        self.host = host
+        self.port = port
+        self.db = db
+        self.repository = {0: {'ok':'ok'}, 1:{}}
+        
+        if connection_pool:
+            self.host = connection_pool.host
+            self.port = connection_pool.port
+            self.db = connection_pool.db
     
     def set(self, key, value):
-        
-        self.repository[key] = value
-        
+       
+        self.repository[self.db][key] = value
         return 'OK'
     
     def get(self, key):
         
-        return self.repository.get(key)
+        return self.repository.get(self.db).get(key)
 
 class RedisConnectionErrorMock(RedisMock):    
     def get(self, key):
@@ -67,11 +79,12 @@ class TestClient(unittest.TestCase):
         
         client.zc.zk.ZooKeeper = ZooKeeperMock
         client.redis.Redis = RedisMock
+        client.redis.ConnectionPool = ConnectionPoolMock
         self.rs = RedisFailover('localhost:8888', '/redis/cluster')
         
         self.rs._setup_redis_master()
         self.rs._setup_redis_slaves()
-
+    
     def test_setup_master_slave(self):
         
         self.assertTrue( isinstance(self.rs.redis_master, RedisMock))
@@ -79,8 +92,7 @@ class TestClient(unittest.TestCase):
 
     def test_set(self):
          
-        resp = self.rs.set('test', 'ok')
-        
+        resp = self.rs.set('test', 'ok')   
         self.assertEquals(resp, 'OK', 'Response ok')
         
     def test_get_ko(self):
@@ -169,8 +181,38 @@ class TestClient(unittest.TestCase):
        
         except ConnectionError, e:
             print e
-
-
+            
+         
+    def test_db_1(self):
+        '''
+            it writes and reads on redis db 1 instead of the default db 0
+        '''
+        
+        # it uses here only the master since there is no propagating of data among redis instances
+        
+        self.rs1 = RedisFailover('localhost:8888', '/redis/cluster', db=1)
+        
+        self.rs1.zk.repository = {'/redis/cluster':{"master":["localhost:6399","OK"],
+                           "slaves":{"0":["localhost:6389","KO"],
+                                     "1":["localhost:6379","KO"]}}}
+        
+        self.rs1._setup_redis_master()
+        self.rs1._setup_redis_slaves()
+        
+        resp = self.rs1.set('db1', 'val_db_1')
+         
+        self.assertEquals(resp, 'OK', 'Response ok')
+        
+        resp = self.rs1.get('db1')
+            
+        self.assertEquals(resp, 'val_db_1', 'Response ok')
+        
+        # now I read on db 0
+        
+        resp = self.rs.get('db1')   
+        self.assertFalse(resp=='val_db_1')
+        
+        
 if __name__ == "__main__":
     #import sys;sys.argv = ['', 'Test.testName']
     unittest.main()
